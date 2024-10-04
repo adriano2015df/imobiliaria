e58207c1f0f3fbb79d9c255077dc84ae342fa5f7
adriano <adriano2015df@gmail.com>
2024-10-04 14:46:08 -0300

    3

--- app/properties/create/page.tsx
+++ app/properties/create/page.tsx
@@ -1,22 +1,22 @@
-"use server"
-import CreateForm from "@/components/create-form"
 
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
+import CreateForm from "@/components/create-form"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+
 
  const  CreatePropertyPage = async () => {
-    const session = await getServerSession()
-    if(!session) {
-        redirect("/")
-     }
+   const session = await getServerSession();
+   if(!session?.user) redirect("/")
+
     return (
         <div className="max-w-md mx-auto mt-5">
-            <h2>{session?.user?.name}</h2>
+            
             <h1 className="text-2x1 text-center mb-2">Adicionar novo Imóvel</h1>
             <CreateForm />
             
         </div>
     )
+
 }
 
 export default CreatePropertyPage
